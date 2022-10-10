fb56f4b367323cebf41523182e5f6272ec83ec15
Ronald Gibb <ronald.gibb@gmail.com>
2022-10-10 14:13:22 +0800

    Update alacritty and i3wm

--- .bin/setbg.sh
+++ .bin/setbg.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-#hsetroot -full ~/Pictures/dracula/arch.png
+#hsetroot -cover ~/Pictures/Wallpapers/retro_computer_aesthetic-wallpaper-3440x1440.jpg -screens 1
 
 hsetroot -solid "#000000"
+hsetroot -full ~/Pictures/Wallpapers/retro_computer_aesthetic-wallpaper-3440x1440.jpg
+
